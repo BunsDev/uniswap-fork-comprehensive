de1fa266dcafe5b8f783538c58038b56138deb67
Interchained <president@worldvaporexpo.com>
2023-08-27 17:37:34 -0400

    forked

--- @uniswap/sdk/dist/constants.d.ts
+++ @uniswap/sdk/dist/constants.d.ts
@@ -17,8 +17,8 @@ export declare enum Rounding {
     ROUND_HALF_UP = 1,
     ROUND_UP = 2
 }
-export declare const FACTORY_ADDRESS = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
-export declare const INIT_CODE_HASH = "0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f";
+export declare const FACTORY_ADDRESS = "0xc3250380E1A5850241925B56f1a344D77334f46A";
+export declare const INIT_CODE_HASH = "0x92048ad0bdd605e50f78d42b9a162f6a2048fce16ebc241e1e132732fe0a47c8";
 export declare const MINIMUM_LIQUIDITY: JSBI;
 export declare const ZERO: JSBI;
 export declare const ONE: JSBI;
